95d93d62ec18e114ee7cb99e9331cb61231cabff
ICYPOLE <2942404587@qq.com>
2020-09-19 22:54:52 +0800

    Updated

--- Easy-Graph Documentation - source/conf.py
+++ Easy-Graph Documentation - source/conf.py
@@ -18,7 +18,7 @@ import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
-project = 'Easy Graph'
+project = 'EasyGraph'
 copyright = '2020, lizheng'
 author = 'lizheng'
 
